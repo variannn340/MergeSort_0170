249dcdd51130588f524f5344b0e33085a4a4da9e
variannn340 <varianvarian0301@gmail.com>
2026-04-07 14:34:43 +0700

    set void input for panjang element array

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -7,3 +7,31 @@ int arr[20], B[20];
 // n is array input size
 int n;
 
+void input()
+{
+     while (true)
+     {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+        cout << "\nMaksimal panjang array adalah 20";
+        }
+
+   }
+   cout << "\n_ _ _ _ _ _ _ _ _ _ _ _ _ _" << endl;
+   cout << "\nInputkan isi elemenet array" << endl;
+   cout << "\n_ _ _ _ _ _ _ _ _ _ _ _ _ _" << endl;
+
+   for (int i = 0; i < n; i++)
+   {
+    cout << "Array index ke- " << i << " : ";
+    cin >> arr[i];
+   }
+}
+
